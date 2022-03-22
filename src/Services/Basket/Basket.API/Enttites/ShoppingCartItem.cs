c160edff3ae0fe658808fd8435257f85e5a881fb
John Cassidy <jpcassidy@outlook.com>
2022-03-22 10:07:24 -0400

    create Basket.API and add api calls and repository crud operations using redis as backend store

---
+++ src/Services/Basket/Basket.API/Enttites/ShoppingCartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Enttites {
+    public class ShoppingCartItem {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using Basket.API.Enttites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
