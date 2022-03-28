523de34e26106ac79dd5589cc960747c3cd7322e
John Cassidy <jpcassidy@outlook.com>
2022-03-28 12:02:08 -0400

    implement Ordering.Infrastructure layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContext> logger) {
+            if (!orderContext.Orders.Any()) {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders() {
+            return new List<Order>
+           {
+                //new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+                new Order() {UserName = "jpc", FirstName = "John", LastName = "Cassidy", EmailAddress = "candiawoods@gmail.com", AddressLine = "Cambridge", Country = "United States", TotalPrice = 350 }
+            };
+        }
+    }
+}
