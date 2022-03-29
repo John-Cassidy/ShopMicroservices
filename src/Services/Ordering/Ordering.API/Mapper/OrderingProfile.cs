2b5653fe6cf533175cf8ddb8cf141e75068a9b4a
John Cassidy <jpcassidy@outlook.com>
2022-03-29 15:01:10 -0400

    subscribe basketcheckout queue message event in ordering basketcheckoutconsumer

---
+++ src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Mapper
+{
+    public class OrderingProfile : Profile {
+        public OrderingProfile() {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
