f2538a50677e79b57ccedde115aa63bf79577a2e
John Cassidy <jpcassidy@outlook.com>
2022-03-30 16:16:36 -0400

    dockerize OcelotApiGw

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
@@ -15,7 +16,7 @@ namespace OcelotApiGw {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            services.AddOcelot();
+            services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
