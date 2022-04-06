42b7aee72be27d162175870ae3d2b0b7b560c5c5
John Cassidy <jpcassidy@outlook.com>
2022-04-06 14:33:58 -0400

    create Common.Logging project for serilog logging

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Serilog.Sinks.Elasticsearch;
 using Serilog;
-using System;
-using System.Reflection;
+using Common.Logging;
 
 namespace AspnetRunBasics {
     public class Program
@@ -15,22 +13,7 @@ namespace AspnetRunBasics {
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, configuration) => {
-                    configuration
-                        .Enrich.FromLogContext()
-                        .Enrich.WithMachineName()
-                        //.WriteTo.Debug()
-                        .WriteTo.Console()
-                        .WriteTo.Elasticsearch(
-                            new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"])) {
-                                IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
-                                AutoRegisterTemplate = true,
-                                NumberOfShards = 2,
-                                NumberOfReplicas = 1
-                            })
-                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)                        
-                        .ReadFrom.Configuration(context.Configuration);
-                })
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
