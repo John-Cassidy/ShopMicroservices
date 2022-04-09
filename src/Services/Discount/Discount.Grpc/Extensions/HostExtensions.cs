6810d4d84024f345f55df80f0a131409ae0c7bfb
John Cassidy <jpcassidy@outlook.com>
2022-04-09 10:36:08 -0400

    add retry pattern for Disount.API and Discount.Grpc database migration for postgres server

--- src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -3,20 +3,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
+using Polly;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Discount.Grpc.Extensions
-{
+namespace Discount.Grpc.Extensions {
     public static class HostExtensions
     {
-        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0) {
-
-            int retryForAvailablility = retry.Value;
-
+        public static IHost MigrateDatabase<TContext>(this IHost host) {            
             using (var scope = host.Services.CreateScope()) {
                 var services = scope.ServiceProvider;
                 var configuration = services.GetRequiredService<IConfiguration>();
@@ -25,45 +18,55 @@ namespace Discount.Grpc.Extensions
                 try {
                     logger.LogInformation("Migrating postresql database.");
 
-                    using var connection = new NpgsqlConnection
-                                       (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                    connection.Open();
+                    var retry = Policy.Handle<NpgsqlException>()
+                           .WaitAndRetry(
+                               retryCount: 5,
+                               sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
+                               onRetry: (exception, retryCount, context) => {
+                                   logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                               });
 
-                    using var command = new NpgsqlCommand {
-                        Connection = connection
-                    };
+                    //if the postgresql server container is not created on run docker compose this
+                    //migration can't fail for network related exception. The retry options for database operations
+                    //apply to transient exceptions   
 
-                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
-                    command.ExecuteNonQuery();
-
-                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
-                                                                ProductName VARCHAR(24) NOT NULL,
-                                                                Description TEXT,
-                                                                Amount INT)";
-                    command.ExecuteNonQuery();
-
-
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
-                    command.ExecuteNonQuery();
-
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
-                    command.ExecuteNonQuery();
-
+                    retry.Execute(() => ExecuteMigrations(configuration));
+                    
                     logger.LogInformation("Migrated postresql database.");
 
                 } catch(NpgsqlException ex) {
                     logger.LogError(ex, "An error occurred while migrating the postresql database");
-
-                    if (retryForAvailablility < 50) {
-                        retryForAvailablility++;
-                        System.Threading.Thread.Sleep(2000);
-                        MigrateDatabase<TContext>(host, retryForAvailablility);
-                    }
                 }
 
             }
 
             return host;
+        }
+
+        private static void ExecuteMigrations(IConfiguration configuration) {
+            using var connection = new NpgsqlConnection
+                                       (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+
+            using var command = new NpgsqlCommand {
+                Connection = connection
+            };
+
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+            command.ExecuteNonQuery();
+
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+            command.ExecuteNonQuery();
         }
     }
 }
