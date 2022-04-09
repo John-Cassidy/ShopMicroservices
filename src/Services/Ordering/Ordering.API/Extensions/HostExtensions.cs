070926f55360e7dc6603f31094b35dd72217875f
John Cassidy <jpcassidy@outlook.com>
2022-04-09 08:50:43 -0400

    add retry pattern for Ordering.API database migration for sql server

--- src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -3,15 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Polly;
 using System;
 
 namespace Ordering.API.Extensions {
     public static class HostExtensions
     {
         public static IHost MigrateDatabase<TContext>(this IHost host,
-                                                    Action<TContext, IServiceProvider> seeder, 
-                                                    int? retry = 0) where TContext : DbContext {
-            int retryForAvailability = retry.Value;
+                                                    Action<TContext, IServiceProvider> seeder) where TContext : DbContext {
 
             using (var scope = host.Services.CreateScope()) {
                 var services = scope.ServiceProvider;
@@ -21,23 +20,28 @@ namespace Ordering.API.Extensions {
                 try {
                     logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
 
-                    InvokeSedder(seeder, context, services);
+                    var retry = Policy.Handle<SqlException>()
+                            .WaitAndRetry(
+                                retryCount: 5,
+                                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 seconds
+                                onRetry: (exception, retryCount, context) => {
+                                    logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                                });
+
+                    //if the sql server container is not created on run docker compose this
+                    //migration can't fail for network related exception. The retry options for DbContext only 
+                    //apply to transient exceptions                    
+                    retry.Execute(() => InvokeSeeder(seeder, context, services));
 
                     logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
                 } catch (SqlException ex) {
-                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
-
-                    if (retryForAvailability < 50) {
-                        retryForAvailability++;
-                        System.Threading.Thread.Sleep(2000);
-                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
-                    }
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);                    
                 }
             }
             return host;
         }
 
-        private static void InvokeSedder<TContext>(Action<TContext, IServiceProvider> seeder, 
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, 
                                                     TContext context, 
                                                     IServiceProvider services) where TContext : DbContext {
             context.Database.Migrate();
