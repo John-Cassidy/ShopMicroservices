e2d94e58a5fd6a9be731c80fde471cd2635ba926
John Cassidy <jpcassidy@outlook.com>
2022-04-11 08:06:54 -0400

    add health check for AspnetRunBasics WebApp

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -1,9 +1,12 @@
 using AspnetRunBasics.Services;
 using Common.Logging;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Polly;
 using Polly.Extensions.Http;
@@ -44,6 +47,9 @@ namespace AspnetRunBasics {
                 .AddPolicyHandler(GetCircuitBreakerPolicy());
 
             services.AddRazorPages();
+
+            services.AddHealthChecks()
+               .AddUrlGroup(new Uri(Configuration["ApiSettings:GatewayAddress"]), "Ocelot API Gw", HealthStatus.Degraded);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,6 +76,11 @@ namespace AspnetRunBasics {
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions() {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
 
